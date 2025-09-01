104908aae71fa1106bced6abaca384d03685de79
MdKhursidAhamad <mdkhursidahamad49@gmail.com>
2025-09-01 13:54:36 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+let scrollbar = document.querySelector(".img-gal");
+let leftbtn = document.getElementById("left-btn");
+let rightbtn = document.getElementById("right-btn");
+
+        scrollbar.addEventListener("wheel", (e) => {
+            e.preventDefault();
+            scrollbar.style.scrollBehavior = "smooth";
+            console.log(scrollbar);
+            scrollbar.scrollLeft += e.deltaY;
+        });
+        leftbtn.addEventListener("click", function () {
+            scrollbar.style.scrollBehavior = "smooth";
+            scrollbar.scrollLeft -= 520;
+
+        })
+        rightbtn.addEventListener("click", function () {
+            scrollbar.style.scrollBehavior = "smooth";
+            scrollbar.scrollLeft += 520;
+
+        })
